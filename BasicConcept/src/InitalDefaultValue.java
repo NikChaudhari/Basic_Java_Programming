aad2ccf5683c663ef893d1b52b53f4a62c4ed1ee
Admin <Admin@DESKTOP-2SELUQ4>
2022-02-05 18:15:10 +0530

    Third Commit

---
+++ BasicConcept/src/InitalDefaultValue.java
@@ -0,0 +1,35 @@
+/**Initail Default value of Data Type 
+ * 
+ */
+
+/**
+ * @author NikChaudhari
+ *
+ */
+public class InitalDefaultValue {
+
+	/**
+	 * @param args
+	 */
+	static byte by;
+	static short sh;
+	static int i;
+	static long l;
+	static float ft;
+	static double db;
+	static boolean bool;
+	static char c;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("by = " + by);
+		System.out.println("sh = " + sh);
+		System.out.println("i = " + i);
+	    System.out.println("l = " + l);
+		System.out.println("ft = " + ft);
+		System.out.println("db = " + db);
+		System.out.println("bool = " + bool);
+	    System.out.println("c = " + c);
+
+	}
+
+}
