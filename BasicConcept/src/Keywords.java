1444ae4695042a6dc7c6ac18612ca3f309b3da44
Admin <Admin@192.168.1.3>
2022-02-05 11:25:17 +0530

    First Commit Keyword

---
+++ BasicConcept/src/Keywords.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * @author Admin
+ *
+ */
+public class Keywords {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Reserved words for data types:");
+		System.out.println("1) byte 5) float 2) short 6) double 3) int 7) char 4) long 8) Boolean");
+		System.out.println("Reserved words for flow control:");
+		System.out.println("1) if 6) for 11) return 2) else 7) do 3) switch 8) while 4) case 9) break 5) default 10) continue");
+		System.out.println("Keywords for modifiers:");
+		System.out.println("1) public 6) abstract 11) volatile 2) private 7) synchronized 3) protected 8) native 4) static 9) strictfp(1.2 version) 5) final 10) transient");
+		System.out.println("Keywords for exception handling:");
+		System.out.println("1) try 4) throw 2) catch 5) throws 3) finally 6) assert");
+		System.out.println("Class related keywords:");
+		System.out.println("1) class 4) extends 2) package 5) implements 3) import 6) interface");
+		System.out.println("Object related keywords:");
+		System.out.println("1) new 3) super 2) instanceof 4) this");
+		System.out.println("Void return type keyword:");
+		System.out.println("1) void");
+	}
+
+}
