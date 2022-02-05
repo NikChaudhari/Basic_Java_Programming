e7d9d8eaf48d34e40133d051dfff6b237559dc30
Admin <Admin@DESKTOP-2SELUQ4>
2022-02-05 18:30:46 +0530

    Comments Program

---
+++ BasicConcept/src/Comments.java
@@ -0,0 +1,8 @@
+// This program is  Comments representation
+public class Comments {
+	public static void main(String[] args) {
+		System.out.println("Single line comment start with //");
+		System.out.println("Multiple line comment starts with /* and ends with */ ");
+	} 
+
+}
