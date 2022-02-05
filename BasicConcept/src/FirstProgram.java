2e0c187ba215e76448bb1855d498c60f7406b794
Admin <Admin@DESKTOP-2SELUQ4>
2022-02-05 18:22:22 +0530

    First Program

---
+++ BasicConcept/src/FirstProgram.java
@@ -0,0 +1,20 @@
+/**This is first java "Hello World" program
+ * 
+ */
+
+/**
+ * @author Admin
+ *
+ */
+public class FirstProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
