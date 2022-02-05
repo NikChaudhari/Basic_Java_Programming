0f507eb011850f780a0b7d2815fa427802b769d6
Admin <Admin@192.168.1.3>
2022-02-05 11:41:45 +0530

    Second Commit

---
+++ BasicConcept/src/PrimitiveDataType.java
@@ -0,0 +1,42 @@
+/**Primitive Data type and its default Value
+ * 
+ */
+
+/**
+ * @author Admin
+ *
+ */
+public class PrimitiveDataType {
+
+	/**
+	 * @param args
+	 */
+	static byte by;
+	static short sh;
+	static int i;
+	static long l;
+	static float ft;
+	static double db;
+	static boolean bool;
+	static char c;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("by = " + by);
+		System.out.println("sh = " + sh);
+		System.out.println("i = " + i);
+	    System.out.println("l = " + l);
+		System.out.println("ft = " + ft);
+		System.out.println("db = " + db);
+		System.out.println("bool = " + bool);
+	    System.out.println("c = " + c);
+	    
+	    
+	    
+	    
+		
+
+	}
+
+}
